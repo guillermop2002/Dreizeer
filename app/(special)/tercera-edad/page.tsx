3066fec13f4f9dcd6e3d120ba94292900db20db3
guillermop2002 <guillermoprieto17@gmail.com>
2025-11-26 18:33:57 +0100

    fix: cambiar texto de descuentos por programas adaptados en tercera edad

--- app/(special)/tercera-edad/page.tsx
+++ app/(special)/tercera-edad/page.tsx
@@ -376,7 +376,7 @@ export default function TerceraEdadPage() {
                     <svg className="w-6 h-6 mr-3 flex-shrink-0 mt-0.5" style={{ color: 'var(--color-brand-primary)' }} fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                     </svg>
-                    <span>Descuentos para grupos de centros municipales</span>
+                    <span>Programas adaptados para centros municipales</span>
                   </li>
                 </ul>
 
